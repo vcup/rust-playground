06c1fefb1541040d7095afffecd00a7a68e76477
vcup <miryo@vcup.moe>
2024-02-03 17:31:03 +0800

    dump avinfo from remote uri

--- rsmpeg/dump_av_info/src/main.rs
+++ rsmpeg/dump_av_info/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::ffi::{CStr, CString};
+use rsmpeg::avformat::AVFormatContextInput;
+
+fn dump_av_info(path: &CStr) -> Result<(), Box<dyn Error>> {
+    let mut input_format_context = AVFormatContextInput::open(path, None, &mut None)?;
+    input_format_context.dump(0, path)?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let cstr = CString::new("https://vcup.moe/Share/SharpMusic.DllHellPTests/SoundSourceTests%600.flac")?;
+    dump_av_info(&cstr)?;
+    Ok(())
 }
